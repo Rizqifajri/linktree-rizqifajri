7fdcb8e9966e3b20405e2aecbab5e9a43e39f51c
Rizqi Fajri <kikifajri08@gmail.com>
2023-09-06 20:31:58 +0700

    update particles

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import './App.css'
 import DeskripsiDiri from './components/DeskripsiDiri'
 import FotoDiri from './components/FotoDiri'
 import List from './components/List'
+import ParticleBg from './components/ParticleBg'
 import {GrGithub, GrInstagram, GrLinkedinOption} from "react-icons/gr"
 
 function App() {
   
   return (
     <>
-    <div className='min-h-screen w-full bg-salt-box-900'>
+    <ParticleBg/>
+    <div className='min-h-screen w-full bg-gray-700'>
     <div className='flex-col justify-center items-center flex px-10'>
       <FotoDiri/>
       <DeskripsiDiri/>
--- src/components/FotoDiri.jsx
+++ src/components/FotoDiri.jsx
@@ -4,7 +4,7 @@ import DeskripsiDiri from './DeskripsiDiri'
 const FotoDiri = () => {
   return (
     <div>
-      <div className="mt-24 flex flex-col group w-full sm:w-[384px] items-center mb-4 transition-all relative">
+      <div className=" mt-36 flex flex-col justify-center group w-full sm:w-[384px] items-center transition-all relative">
         <div className="bg-[#ffffff] scale-125 w-[120px] rounded-full border-2 cursor-pointer border-my-gray-200 group-hover:border-4	 group-hover:border-my-gray-800 transition-all aspect-square object-contain overflow-hidden z-50">
         <img className='scale-125 mt-3'
 					
