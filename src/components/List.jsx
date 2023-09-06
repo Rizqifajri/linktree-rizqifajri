93e826788972227bfe63a61c8466f3e6a2dc242a
Rizqi Fajri <kikifajri08@gmail.com>
2023-09-06 18:23:32 +0700

    update link

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -7,17 +7,17 @@ const List = () => {
     <div className='flex flex-col w-full group items-center mb-4 transition-all relative '>
 
       <a className='text-white  hover:text-black bg-gradient-to-r  from-[#e1d5e9] via-[#76e9fd] to-[#e15bf3] text-lg hover:scale-110 sm:w-96 transition-all w-full hover:bg-[#24292F]/90 font-medium rounded-lg px-5 py-3  mb-2 flex justify-center items-center gap-2 min-w-fit'
-      href="">Github
+      href="https://github.com/Rizqifajri">Github
       <GrGithub/>
       </a>
       
       <a className='text-white  hover:text-black bg-gradient-to-r  from-[#eedbfa] via-[#76e9fd] to-[#e15bf3] text-lg hover:scale-110 sm:w-96 transition-all w-full hover:bg-[#24292F]/90 font-medium rounded-lg px-5 py-3  mb-2 flex justify-center items-center gap-2 min-w-fit'
-      href="">Linkedn
+      href="https://www.linkedin.com/in/muhammad-rizqi-fajri-0759a222b/">Linkedn
       <GrLinkedinOption/>
       </a>
       
       <a className='text-white hover:text-black bg-gradient-to-r  from-[#eedbfa] via-[#73daec] to-[#e15bf3] text-lg hover:scale-110 sm:w-96 transition-all w-full hover:bg-[#24292F]/90 font-medium rounded-lg px-5 py-3  mb-2 flex justify-center items-center gap-2 min-w-fit'
-      href="">Instagram
+      href="https://www.instagram.com/rizqifajriii/">Instagram
       <GrInstagram/>
       
       </a>
